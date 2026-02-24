5b7c767d3af5414a341cf87e8c74231c0feb9e5d
BP <53974728+bala9424@users.noreply.github.com>
2026-02-24 21:35:34 +0530

    Update 01_Max product of three.js

--- problem_solving_coding/problem_solving/array(s)/01_Max product of three.js
+++ problem_solving_coding/problem_solving/array(s)/01_Max product of three.js
@@ -1,3 +1,19 @@
+
+//approach 
+/*
+To get the maximum product of 3 numbers, there are only two possible best cases:
+
+Three largest positive numbers
+Example: [2, 5, 10, 20] → 20 × 10 × 5
+
+Two very small (negative) numbers + one largest positive
+Because: negative × negative = positive
+Example: [-100, -5, 10, 2] → (-100) × (-5) × 10 = 5000 (bigger!)
+
+So the answer is always:
+
+max( largest1 × largest2 × largest3 , smallest1 × smallest2 × largest1 )
+*/
 function maxProductOfThree(nums) {
   if (nums.length < 3) {
     throw new Error("Array must have at least 3 numbers");
